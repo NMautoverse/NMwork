0b5058ed153446dacb83df970928cbdaac65154b
shroff <Sanaya_Shroff@vrtx.com>
2026-08-04 16:06:16 -0400

    fix to printParameterTable to fix backslash bug causing errors in table header when in latex/kable mode, also spacing width issues to not overwrite Rmd footers

--- R/printParameterTable.R
+++ R/printParameterTable.R
@@ -266,10 +266,20 @@ printParameterTable <- function(pars,engine="kable",format,footnotes=NULL,script
 
 
         if(dt.conf$format=="latex"){
-            ## colnames(partab2) <- latexify(colnames(partab2))
-            cnames <- copy(colnames(partab2))
-            cnames[!grepl("^ *$",cnames)] <- latexify(cnames[!grepl("^ *$",cnames)])
-            colnames(partab2) <- cnames
+            ## The multi-line headers are authored with LaTeX markup that must
+            ## survive verbatim because the kable() call below uses escape=FALSE.
+            ## Routing them through latexify() escaped the backslash (\newline ->
+            ## literal "\textbackslash newline") and broke the header layout.
+            ## Set the LaTeX-ready headers directly instead. NOTE: do NOT use
+            ## \makecell here -- these are tabu "X" (paragraph) columns and
+            ## \makecell inside a tabu X-cell triggers "Improper \prevdepth" at
+            ## \end{tabu}. A bare \newline breaks the line cleanly in an X column.
+            colnames(partab2) <- c(
+                "  ",
+                " ",
+                "Estimate (RSE\\%) \\newline [CV\\% or Corr\\%]",
+                "95\\% Confidence \\newline Interval",
+                "panel.label")
         }
     }
 
@@ -307,7 +317,7 @@ printParameterTable <- function(pars,engine="kable",format,footnotes=NULL,script
             kable_styling(full_width = T,
                           font_size = 7.5,  latex_options = "HOLD_position") |>
             column_spec(1, width="4em")|>
-            column_spec(2, width = "26em") |>
+            column_spec(2, width = "26em")|>
             column_spec(3, width = "12em")
 
         
